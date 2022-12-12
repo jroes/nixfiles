787b0756788073eacdd62b861a55b77085d28728
Jon Roes <jon.roes@snowflake.com>
2022-12-12 11:17:46 -0500

    add in a nicely themed tmux

--- home.nix
+++ home.nix
@@ -36,4 +36,25 @@
       pkgs.vimPlugins.vim-nix
     ];
   };
+
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      yank
+      {
+        plugin = dracula;
+        extraConfig = ''
+          set -g @dracula-show-battery false
+          set -g @dracula-show-powerline true
+          set -g @dracula-refresh-rate 10
+        '';
+      }
+    ];
+
+    extraConfig = ''
+      set -g mouse on
+    '';
+  };
 }
